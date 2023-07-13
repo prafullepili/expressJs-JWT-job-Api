45ea2fe0c570ff8098888b34917c45e7fe35dc8e
Prafull Epili <prafullepili@gmail.com>
2023-07-13 21:15:42 +0530

    added pre save signal

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,10 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes')
 const { BadRequestError } = require('../errors')
-const bcrypt = require('bcryptjs')
 
 
 const register = async (req, res, next) => {
-    const { name, email, password } = req.body
-    const salt = await bcrypt.genSalt(10);
-    const hasedPassword = await bcrypt.hash(password, salt);
-    const tempUser = { name, email, password: hasedPassword }
-    const user = await User.create({ ...tempUser })
+    const user = await User.create({ ...req.body })
     res.status(StatusCodes.CREATED).json({ user })
 }
 
