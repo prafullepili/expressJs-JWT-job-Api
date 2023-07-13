12cce6aa4cbe4af8e6586181b0446923577c2e1b
Prafull Epili <prafullepili@gmail.com>
2023-07-13 21:13:36 +0530

    password hashing done

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,19 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError } = require('../errors')
+const bcrypt = require('bcryptjs')
 
 
-const register = async(req, res) => {
-    res.send("register user")
+const register = async (req, res, next) => {
+    const { name, email, password } = req.body
+    const salt = await bcrypt.genSalt(10);
+    const hasedPassword = await bcrypt.hash(password, salt);
+    const tempUser = { name, email, password: hasedPassword }
+    const user = await User.create({ ...tempUser })
+    res.status(StatusCodes.CREATED).json({ user })
 }
 
-const login = async(req,res)=> {
+const login = async (req, res) => {
     res.send('login user')
 }
 
