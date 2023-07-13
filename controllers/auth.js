4a4c4d1519acc03e92f466c1406414e56a6f0764
Prafull Epili <prafullepili@gmail.com>
2023-07-13 21:49:48 +0530

    added token generator

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,14 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes')
 const { BadRequestError } = require('../errors')
-
+const jwt = require('jsonwebtoken')
 
 const register = async (req, res, next) => {
     const user = await User.create({ ...req.body })
-    res.status(StatusCodes.CREATED).json({ user })
+    console.log(user.name)
+    const token = jwt.sign({ userId: user._id, name: user.name }, "jwtSecret", { expiresIn: '30d' })
+
+    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
 
 const login = async (req, res) => {
