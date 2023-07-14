2e0ad6b3503df583f6c33b28d9e4ad05ff2b5f51
Prafull Epili <prafullepili@gmail.com>
2023-07-14 12:56:43 +0530

    added local mongoConnectString

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(express.json());
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/jobs", jobsRoutes);
 
+// middleware 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
@@ -28,7 +29,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI)
+    await connectDB(process.env.MONGO_URI_LOCAL)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
