98ceeb4b0b1e5310ed98e1f78e6c10005ee5fa92
Prafull Epili <prafullepili@gmail.com>
2023-07-15 02:07:00 +0530

    getJob implementation done

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -14,7 +14,14 @@ const getAllJobs = async (req, res) => {
 }
 
 const getJob = async (req, res) => {
-    res.send("get job")
+    const { user: { userId }, params: { id: jobId } } = req
+    const job = await Job.findOne({
+        _id: jobId, createdBy: userId
+    })
+    if (!job) {
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json(job)
 }
 
 const updateJob = async (req, res) => {
