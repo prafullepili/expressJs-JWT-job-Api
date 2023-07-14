032e707992569494debde359b830e8bb6b46a23d
Prafull Epili <prafullepili@gmail.com>
2023-07-14 17:51:02 +0530

    added login validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,7 @@ const { BadRequestError, UnauthenticatedError } = require('../errors')
 const jwt = require('jsonwebtoken')
 
 
-const register = async (req, res, next) => {
+const register = async (req, res) => {
     const user = await User.create({ ...req.body })
     const token = user.createJWT();
     res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
@@ -13,10 +13,9 @@ const register = async (req, res, next) => {
 
 const login = async (req, res) => {
     const { email, password } = req.body;
-
-    // if (!email || !password) {
-    //     throw new BadRequestError("Please provide email and password");
-    // }
+    if (!email || !password) {
+        throw new BadRequestError("Please provide email and password");
+    }
     const user = await User.findOne({ email })
     if (!user) {
         throw new UnauthenticatedError("Invalid Credentials")
