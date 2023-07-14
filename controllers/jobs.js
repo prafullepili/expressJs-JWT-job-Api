feef62e2ccfde4d78eb31b13ba8239dfa7ed494e
Prafull Epili <prafullepili@gmail.com>
2023-07-14 19:23:55 +0530

    createJob functionality done

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,4 +1,13 @@
+const Job = require('../models/Job')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, NotFoundError } = require("../errors")
 
+const createJob = async (req, res) => {
+    console.log(req.user)
+    req.body.createdBy = req.user.userId;
+    const job = await Job.create({ ...req.body })
+    res.status(StatusCodes.CREATED).json({ job })
+}
 
 const getAllJobs = async (req, res) => {
     res.send("get all jobs")
@@ -8,9 +17,6 @@ const getJob = async (req, res) => {
     res.send("get job")
 }
 
-const createJob = async (req, res) => {
-    res.send('create job')
-}
 const updateJob = async (req, res) => {
     res.send('update job')
 }
