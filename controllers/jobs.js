e7adbae89be00167424d6580f703d49bfe9c74b6
Prafull Epili <prafullepili@gmail.com>
2023-07-15 11:59:49 +0530

    updateJob and deleteJob done

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,6 +1,7 @@
 const Job = require('../models/Job')
 const { StatusCodes } = require('http-status-codes')
-const { BadRequestError, NotFoundError } = require("../errors")
+const { BadRequestError, NotFoundError } = require("../errors");
+const { json } = require('express');
 
 const createJob = async (req, res) => {
     req.body.createdBy = req.user.userId;
@@ -25,10 +26,40 @@ const getJob = async (req, res) => {
 }
 
 const updateJob = async (req, res) => {
-    res.send('update job')
+    const {
+        body: { company, position },
+        user: { userId },
+        params: { id: jobId } } = req;
+    if (company === "" || position === "") {
+        throw new BadRequestError('Company or Position fields cannot be empty')
+    }
+    const job = await Job.findOneAndUpdate(
+        { _id: jobId, createdBy: userId },
+        req.body,
+        { new: true, runValidators: true }
+    )
+    if (!job) {
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({ job })
 }
+
 const deleteJob = async (req, res) => {
-    res.send('delete job')
+    const {
+        user: { userId },
+        params: { id: jobId }
+    } = req
+    const job = await Job.findByIdAndRemove({
+        _id: jobId,
+        createdBy: userId
+    })
+
+    if (job) {
+        res.status(StatusCodes.OK).send({ msg: "success" })
+    }
+    else {
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
 }
 
 module.exports = {
