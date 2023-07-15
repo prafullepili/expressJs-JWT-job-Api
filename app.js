62ed880ef4fb12430ade12f3d98d3d996478aca6
Prafull Epili <prafullepili@gmail.com>
2023-07-15 19:27:54 +0530

    added helmet, cors, xss, rateLimiter to reduce api hit limit

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
 require('express-async-errors');
+
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const express = require('express');
 const app = express();
 
@@ -9,7 +15,7 @@ const connectDB = require('./db/connect');
 // auth middleware
 const authenticateUser = require('./middleware/authentication');
 
-//routers
+// rautes imported
 const authRoutes = require('./routes/auth');
 const jobsRoutes = require('./routes/jobs');
 
@@ -17,10 +23,18 @@ const jobsRoutes = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+// 
+app.set('trust proxy', 1);
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+}))
 app.use(express.json());
-// extra packages
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
-// routes
+// routes used
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/jobs", authenticateUser, jobsRoutes);
 
